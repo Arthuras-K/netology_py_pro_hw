dedfe9b652c133a7763a206a61222a5f638d0535
Arthuras Kiatris <a-r-ch@yandex.ru>
2022-09-11 22:52:38 +0300

    finish hw2

--- netology_py_pro_hw_2_reg/main.py
+++ netology_py_pro_hw_2_reg/main.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import csv
 import re
+from xmlrpc.client import Boolean
 
 # читаем адресную книгу в формате CSV в список contacts_list
 with open("netology_py_pro_hw_2_reg/phonebook_raw.csv", 'r', encoding='UTF-8') as f:
@@ -8,35 +9,37 @@ with open("netology_py_pro_hw_2_reg/phonebook_raw.csv", 'r', encoding='UTF-8') a
     contacts_list = list(rows)
 
 
-# TODO 1: выполните пункты 1-3 ДЗ
+# TODO 1: пункты 1-3 ДЗ
+
+# Функция проверяет на наличие контакта в списке и слияние их
+def check_repeat(main_list: list, add_list: list)-> Boolean:
+    flag = True
+    for ml in main_list:        
+        if ml[0] == add_list[0] and ml[1] == add_list[1]:
+            flag = False 
+            for n in range(2,7): 
+                    if  ml[n] == '':
+                        ml[n] = result[n]   
+    return flag   
+
 push_list = [contacts_list[0]]
 
 for i in range(1, len(contacts_list)):
     contact = ','.join(contacts_list[i])
 
-    pattern_phone = r'(\+7|8)\W{0,2}(\d{3})\W{0,2}(\d{3})\W?(\d\d)\W?(\d\d)\W{0,2}(доб\.)?\W{0,2}(\d{4})?\)?'
+    pattern_phone = r'(\+7|8)\W{0,2}(\d{3})\W{0,2}(\d{3})\W?(\d\d)\W?(\d\d)(?:\W{0,2}(доб\.)\W{0,2}(\d{4}))?\)?'
     standard_phone = r'+7(\2)\3-\4-\5 \6\7'
     contact_fix = re.sub(pattern_phone, standard_phone, contact)    
+    print(f'Телефонный номер {i} контакта отредактирован')
 
     pattern = r"^(\w+)[ ,](\w+)[ ,](\w*),+(\w*),+(.*[^,])?,+(.*),+(.*)"
     result = list(re.findall(pattern, contact_fix)[0])
 
-    pprint(result)
-
-    flag = True
-
-    for pl in push_list:        
-        if pl[0] == result[0] and pl[1] == result[1]:
-            flag = False 
-            break
-
-    if flag:
+    if check_repeat(push_list, result):
         push_list.append(result)
+        print(f'Контакт {i} добавлен')
     else:
-        for j in range(2,7): 
-            if  pl[j] == '':
-                pl[j] = result[j]
-
+        print(f'Контакт {i} объединен')
 
 # TODO 2: сохраните получившиеся данные в другой файл
 
@@ -44,3 +47,4 @@ for i in range(1, len(contacts_list)):
 with open("netology_py_pro_hw_2_reg/phonebook.csv", "w", encoding='UTF-8') as f:
     datawriter = csv.writer(f, delimiter=',', lineterminator='\n')
     datawriter.writerows(push_list)
+    print('\nФайл успешно записан в формате CSV\n')

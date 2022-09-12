442376ca2629671d1c129a16f5bd3288c58c9b8c
Arthuras Kiatris <a-r-ch@yandex.ru>
2022-09-13 00:17:57 +0300

    version 1

--- netology_py_pro_hw_3_web_scraping/main.py
+++ netology_py_pro_hw_3_web_scraping/main.py
@@ -2,32 +2,41 @@ import bs4
 import requests
 from fake_useragent import UserAgent
 
-HUBS = ['Физика', 'Дизайн', 'ty']
 
+
+
+# фиктивные заголовки для обхода защиты сайта от парсинга 
 HEADERS = {
     'User-Agent': UserAgent().random
     }
 
+# определяем список ключевых слов
+KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+
+# адрес сайта для парсинга
 base_url = "https://habr.com"
 url = base_url + '/ru/all/'
 
-
-print(HEADERS)
-
-
+# отправляем запрос к сайту
 response = requests.get(url, headers=HEADERS)
+# получаем ответ и применяем метод text выходит весь запутанный код страницы в HTML
 text = response.text
 
-
+# превращаем сложный код в красивый суп
 soup = bs4.BeautifulSoup(text, features="html.parser")
 
-articles = soup.find_all("article")
+# узнав в каком контейнере находится нужная информайция, начинаем к ней копать через find\find_all
+articles = soup.find_all(class_="tm-articles-list__item")
 
 for article in articles:
-    hubs = article.find_all(class_="tm-article-snippet__hubs-item")
-    hubs = [hub.text.strip() for hub in hubs]
-    for hub in HUBS:
-        title = article.find('h2').find('span').text
-        href = article.find(class_='tm-article-snippet__title-link').attrs['href']
-        link = base_url+ href
-        print(f"{title} ---> {link}")
+    headline = article.find(class_="tm-article-snippet__title-link").find('span').text
+    print(111111111111111111, headline)
+
+    preview_info = article.find(class_="article-formatted-body article-formatted-body article-formatted-body_version-2").find('p')
+    print(333333333333333333, preview_info)
+    # hubs = [hub.text.strip() for hub in hubs]
+    # for hub in HUBS:
+    #     title = article.find('h2').find('span').text
+    #     href = article.find(class_='tm-article-snippet__title-link').attrs['href']
+    #     link = base_url+ href
+    #     print(f"{title} ---> {link}")

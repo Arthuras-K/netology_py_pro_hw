772a93e9e0f59185bf9b4ddfd63ac56b194e3a85
Arthuras <a-r-ch@yandex.ru>
2022-09-19 11:53:47 +0300

    finish hw4

--- netology_py_pro_hw_4_iterator_generator/def_hw_4.py
+++ netology_py_pro_hw_4_iterator_generator/def_hw_4.py
@@ -1,5 +1,7 @@
 class FlatIterator:
-    
+    '''
+    Класс итератор для вывода элементов 2D списка
+    '''    
     def __init__(self, nested_list: list):
         self.nested_list = nested_list
 
@@ -17,9 +19,55 @@ class FlatIterator:
 
         return flat_list[self.index]
 
+# Функции генератор для вывода элементов 2D списка
 def flat_generator(nested_list: list) -> list:
     flat_list = [el for item in nested_list for el in item]
     index = 0
     while index < len(flat_list):
         yield flat_list[index]
         index += 1
+
+
+class DeepIterator:
+    '''
+    Класс итератор для вывода элементов списка с любым уровнем вложенности
+    '''       
+    def __init__(self, deep_list: list):
+        self.deep_list = deep_list
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        def diving(sea_list):
+            if isinstance(sea_list[0], list) and sea_list[0]:
+                sea_list = sea_list[0]
+                return diving(sea_list)
+            else:
+                return sea_list.pop(0)
+
+        while True:    
+            if self.deep_list:    
+                result = diving(self.deep_list)
+                if result != []:
+                    return result
+            else:
+                raise StopIteration
+
+
+    
+# Функции генератор для вывода элементов списка с любым уровнем вложенности
+def deep_generator(deep_list: list) -> list:
+    def diving(sea_list):
+        if isinstance(sea_list[0], list) and sea_list[0]:
+            sea_list = sea_list[0]
+            return diving(sea_list)
+        else:
+            return sea_list.pop(0)
+
+    while True:    
+        if deep_list:    
+            result = diving(deep_list)
+            if result != []:
+                yield result
--- netology_py_pro_hw_4_iterator_generator/main.py
+++ netology_py_pro_hw_4_iterator_generator/main.py
@@ -1,5 +1,8 @@
+from copy import deepcopy
 from def_hw_4 import FlatIterator
 from def_hw_4 import flat_generator
+from def_hw_4 import DeepIterator
+from def_hw_4 import deep_generator
 
 
 nested_list = [
@@ -8,10 +11,19 @@ nested_list = [
     [1, 2, None],
 ]
 
+deep_list = [
+    ['start', 's',['t', ['a'], 'r'], 't'],
+	['a', 'b', 'c', [1, [[[[2]]]], 3]],
+    [4],
+	[False, 'or', None],
+    [['finish']]
+]
+
+
+
 if __name__ == '__main__':
     # task 1
-    # Написать итератор, который принимает список списков, и возвращает их плоское представление
-
+    # Написать итератор, который принимает список списков и возвращает их плоское представление
     print('task 1.1:')
     for item in FlatIterator(nested_list):
         print(item)
@@ -19,8 +31,26 @@ if __name__ == '__main__':
     flat_list = [item for item in FlatIterator(nested_list)]
     print('\ntask 1.2:\n', flat_list)
 
+
     # task 2
-    # Написать генератор, который принимает список списков, и возвращает их плоское представление
+    # Написать генератор, который принимает список списков и возвращает их плоское представление
     print('\ntask 2:')
     for item in  flat_generator(nested_list):
         print(item)
+
+
+    # task 3*
+    # Написать итератор, который принимает список c сюбым уровнем вложенности и возвращает их плоское представление
+    print('\ntask 3:')
+    copy_list = deepcopy(deep_list)
+    for item in DeepIterator(copy_list):
+        print(item)
+
+
+    # task 4*
+    # Написать генератор, который принимает список с любым уровнем вложенности и возвращает их плоское представление
+    print('\ntask 4:')
+    copy_list = deepcopy(deep_list)
+    for item in  deep_generator(copy_list):
+        print(item)
+
